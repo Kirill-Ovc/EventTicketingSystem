c8276de47fc7e1d6884392283801afe422a4b6a7
Kirill Ovcharenko <kirill.056@gmail.com>
2024-05-24 09:31:26 +0300

    Remove project ConsoleTest

--- EventTicketingSystem.ConsoleTest/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using EventTicketingSystem.ConsoleTest;
-
-var seedService = ServiceProvider.GetSeedService();
-await seedService.SeedAllData();
-
-var userRepository = ServiceProvider.GetUserRepository();
-var result = await userRepository.GetUsers();
-
-Console.WriteLine(result.First().Name);
