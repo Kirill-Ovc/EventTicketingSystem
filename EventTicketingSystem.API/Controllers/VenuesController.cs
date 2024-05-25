2d20df2c09c60bbf3fd78d5bb844d74d70f38031
Kirill Ovcharenko <kirill.056@gmail.com>
2024-05-25 14:06:10 +0300

    Add VenuesController and unit tests

---
+++ EventTicketingSystem.API/Controllers/VenuesController.cs
@@ -0,0 +1,51 @@
+﻿using EventTicketingSystem.DataAccess.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventTicketingSystem.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VenuesController : ControllerBase
+    {
+        private readonly IVenueRepository _venueRepository;
+        private readonly ISeatRepository _seatRepository;
+
+        public VenuesController(
+            IVenueRepository venueRepository,
+            ISeatRepository seatRepository)
+        {
+            _venueRepository = venueRepository;
+            _seatRepository = seatRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _venueRepository.GetVenues();
+            return Ok(users);
+        }
+
+        [HttpGet("{city_id}")]
+        public async Task<IActionResult> GetByCity(int cityId)
+        {
+            if (cityId == default)
+            {
+                return BadRequest("City ID is required");
+            }
+            var venue = await _venueRepository.GetVenuesByCity(cityId);
+            return Ok(venue);
+        }
+
+        [HttpGet("{venue_id}/sections")]
+        public async Task<IActionResult> GetSections(int venueId)
+        {
+            if (venueId == default)
+            {
+                return BadRequest("Venue ID is required");
+            }
+            var venue = await _seatRepository.GetSections(venueId);
+            return Ok(venue);
+        }
+
+    }
+}
---
+++ EventTicketingSystem.Tests/Controllers/VenuesControllerTests.cs
@@ -0,0 +1,121 @@
+﻿using AutoFixture;
+using EventTicketingSystem.API.Controllers;
+using EventTicketingSystem.DataAccess.Interfaces;
+using EventTicketingSystem.DataAccess.Models.Entities;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+
+namespace EventTicketingSystem.Tests.Controllers
+{
+    [TestFixture]
+    public class VenuesControllerTests
+    {
+        private VenuesController _controller;
+        private IVenueRepository _venueRepository;
+        private ISeatRepository _seatRepository;
+        private readonly IFixture _fixture = new Fixture();
+
+        public VenuesControllerTests()
+        {
+            _fixture.Customize<Section>(s => s
+                .Without(b => b.Venue)
+                .Without(b => b.Seats));
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _venueRepository = Substitute.For<IVenueRepository>();
+            _seatRepository = Substitute.For<ISeatRepository>();
+            _controller = new VenuesController(_venueRepository, _seatRepository);
+        }
+
+        [Test]
+        public async Task VenuesController_Get_ReturnsVenues()
+        {
+            // Arrange
+            var venues = _fixture.CreateMany<Venue>(10).ToList();
+            _venueRepository.GetVenues().Returns(venues);
+
+            // Act
+            var response = await _controller.Get();
+            var okResult = response as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(okResult);
+            okResult.StatusCode.Should().Be(StatusCodes.Status200OK);
+            okResult.Value.Should().BeEquivalentTo(venues);
+        }
+
+        [Test]
+        public async Task VenuesController_GetByCity_ReturnsVenuesByCity()
+        {
+            // Arrange
+            var city = _fixture.Create<int>();
+            var venues = _fixture.CreateMany<Venue>(5).ToList();
+            _venueRepository.GetVenuesByCity(city).Returns(venues);
+
+            // Act
+            var response = await _controller.GetByCity(city);
+            var okResult = response as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(okResult);
+            okResult.StatusCode.Should().Be(StatusCodes.Status200OK);
+            okResult.Value.Should().BeEquivalentTo(venues);
+        }
+
+        [Test]
+        //GetSections
+        public async Task VenuesController_GetSections_ReturnsSections()
+        {
+            // Arrange
+            var venueId = _fixture.Create<int>();
+            var sections = _fixture.CreateMany<Section>(5).ToList();
+            _seatRepository.GetSections(venueId).Returns(sections);
+
+            // Act
+            var response = await _controller.GetSections(venueId);
+            var okResult = response as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(okResult);
+            okResult.StatusCode.Should().Be(StatusCodes.Status200OK);
+            okResult.Value.Should().BeEquivalentTo(sections);
+        }
+
+        [Test]
+        public async Task VenuesController_GetSections_WhenVenueIdIsZero_ReturnsBadRequest()
+        {
+            // Arrange
+            var venueId = 0;
+
+            // Act
+            var response = await _controller.GetSections(venueId);
+            var badRequestResult = response as BadRequestObjectResult;
+
+            // Assert
+            Assert.NotNull(badRequestResult);
+            badRequestResult.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+            badRequestResult.Value.Should().Be("Venue ID is required");
+        }
+
+        [Test]
+        public async Task VenuesController_GetByCity_WhenCityIdIsZero_ReturnsBadRequest()
+        {
+            // Arrange
+            var cityId = 0;
+
+            // Act
+            var response = await _controller.GetByCity(cityId);
+            var badRequestResult = response as BadRequestObjectResult;
+
+            // Assert
+            Assert.NotNull(badRequestResult);
+            badRequestResult.StatusCode.Should().Be(StatusCodes.Status400BadRequest);
+            badRequestResult.Value.Should().Be("City ID is required");
+        }
+    }
+}
