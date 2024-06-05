2ca43af9b026b80c90eec64a5711a9333f12d7eb
Kirill Ovcharenko <kirill.056@gmail.com>
2024-06-05 21:35:28 +0300

    Add tests

---
+++ EventTicketingSystem.Tests/Services/BookingCartMapperTests.cs
@@ -0,0 +1,79 @@
+﻿using EventTicketingSystem.API.Models;
+using EventTicketingSystem.API.Services;
+using EventTicketingSystem.DataAccess.Interfaces;
+using EventTicketingSystem.DataAccess.Models.Entities;
+using EventTicketingSystem.DataAccess.Models.Enums;
+using FluentAssertions;
+using NSubstitute;
+
+namespace EventTicketingSystem.Tests.Services
+{
+    [TestFixture]
+    public class BookingCartMapperTests
+    {
+        private BookingCartMapper _service;
+        private IBookingSeatRepository _bookingSeatRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bookingSeatRepository = Substitute.For<IBookingSeatRepository>();
+            _service = new BookingCartMapper(_bookingSeatRepository);
+        }
+
+        [Test]
+        public async Task MapBookingToCart_WithBooking_ReturnsCart()
+        {
+            // Arrange
+            _bookingSeatRepository.GetSeats(_booking.Id).Returns(_bookingSeats);
+
+            // Act
+            var result = await _service.MapBookingToCart(_booking);
+
+            // Assert
+            result.Should().BeEquivalentTo(_cart);
+        }
+
+        #region TestData
+        private readonly Booking _booking = new Booking
+        {
+            Id = 111,
+            Uuid = "cartId",
+            UserId = 1,
+            Status = BookingStatus.Active,
+            ExpirationTimeStamp = DateTime.Today.AddDays(1)
+        };
+        private readonly Cart _cart = new Cart
+        {
+            CartId = "cartId",
+            UserId = 1,
+            Status = "Active",
+            ExpirationTimeStamp = DateTime.Today.AddDays(1),
+            CartItems = new List<CartItem>
+            {
+                new CartItem
+                {
+                    EventId = 1,
+                    EventSeatId = 1,
+                    EventName = "Event 1",
+                    Price = 10
+                },
+                new CartItem
+                {
+                    EventId = 1,
+                    EventSeatId = 2,
+                    EventName = "Event 1",
+                    Price = 20
+                }
+            }
+        };
+        private static readonly EventSeat _eventSeatInfo =
+            new EventSeat { EventId = 1, Event = new Event { Name = "Event 1" } };
+        private static readonly List<BookingSeat> _bookingSeats = new List<BookingSeat>
+        {
+            new() { EventSeatId = 1, Price = 10, EventSeat = _eventSeatInfo },
+            new() { EventSeatId = 2, Price = 20, EventSeat = _eventSeatInfo }
+        };
+        #endregion
+    }
+}
---
+++ EventTicketingSystem.Tests/Services/BookingSeatServiceTests.cs
@@ -0,0 +1,142 @@
+﻿using EventTicketingSystem.API.Exceptions;
+using EventTicketingSystem.API.Services;
+using EventTicketingSystem.DataAccess.Interfaces;
+using EventTicketingSystem.DataAccess.Models.Entities;
+using EventTicketingSystem.DataAccess.Models.Enums;
+using FluentAssertions;
+using NSubstitute;
+
+namespace EventTicketingSystem.Tests.Services
+{
+    [TestFixture]
+    public class BookingSeatServiceTests
+    {
+        private BookingSeatService _service;
+        private IBookingSeatRepository _bookingSeatRepository;
+        private IOfferRepository _offerRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bookingSeatRepository = Substitute.For<IBookingSeatRepository>();
+            _offerRepository = Substitute.For<IOfferRepository>();
+            _service = new BookingSeatService(_bookingSeatRepository, _offerRepository);
+        }
+
+        [Test]
+        public async Task AddSeat_SeatBooked_ThrowsException()
+        {
+            // Arrange
+            var bookingId = 1;
+            var eventSeatId = 1;
+            var offerId = 1;
+            _bookingSeatRepository.GetSeat(eventSeatId).Returns(new BookingSeat());
+
+            // Act
+            Func<Task> action = async () => await _service.AddSeat(bookingId, eventSeatId, offerId);
+
+            // Assert
+            await action.Should().ThrowAsync<BusinessException>().WithMessage("Seat is already booked");
+            await _bookingSeatRepository.Received(1).GetSeat(eventSeatId);
+        }
+
+        [Test]
+        public async Task AddSeat_OfferNotFound_ThrowsException()
+        {
+            // Arrange
+            var bookingId = 1;
+            var eventSeatId = 1;
+            var offerId = 1;
+            _bookingSeatRepository.GetSeat(eventSeatId).Returns((BookingSeat)null);
+            _offerRepository.Find(offerId).Returns((Offer)null);
+
+            // Act
+            Func<Task> action = async () => await _service.AddSeat(bookingId, eventSeatId, offerId);
+
+            // Assert
+            await action.Should().ThrowAsync<BusinessException>().WithMessage("Price offer not found. OfferId = 1");
+            await _offerRepository.Received(1).Find(offerId);
+        }
+
+        [Test]
+        public async Task AddSeat_ValidOffer_AddsSeat()
+        {
+            // Arrange
+            var bookingId = 1;
+            var eventSeatId = 1;
+            var offerId = 1;
+            _bookingSeatRepository.GetSeat(eventSeatId).Returns((BookingSeat)null);
+            _offerRepository.Find(offerId).Returns(new Offer { Price = 100, TicketLevel = TicketLevel.Vip });
+
+            // Act
+            await _service.AddSeat(bookingId, eventSeatId, offerId);
+
+            // Assert
+            await _bookingSeatRepository.Received(1).Add(Arg.Is<BookingSeat>(x => 
+                x.BookingId == bookingId && 
+                x.EventSeatId == eventSeatId && 
+                x.Price == 100 && 
+                x.TicketLevel == TicketLevel.Vip));
+        }
+
+        [Test]
+        public async Task RemoveSeat_SeatNotBooked_ThrowsException()
+        {
+            // Arrange
+            var bookingId = 1;
+            var eventSeatId = 1;
+            _bookingSeatRepository.GetSeat(eventSeatId).Returns((BookingSeat)null);
+
+            // Act
+            Func<Task> action = async () => await _service.RemoveSeat(bookingId, eventSeatId);
+
+            // Assert
+            await action.Should().ThrowAsync<BusinessException>().WithMessage("Seat is not booked");
+            await _bookingSeatRepository.Received(1).GetSeat(eventSeatId);
+        }
+
+        [Test]
+        public async Task RemoveSeat_SeatNotInCart_ThrowsException()
+        {
+            // Arrange
+            var bookingId = 1;
+            var eventSeatId = 1;
+            _bookingSeatRepository.GetSeat(eventSeatId).Returns(new BookingSeat { BookingId = 2 });
+
+            // Act
+            Func<Task> action = async () => await _service.RemoveSeat(bookingId, eventSeatId);
+
+            // Assert
+            await action.Should().ThrowAsync<BusinessException>().WithMessage("Seat is not in the cart");
+            await _bookingSeatRepository.Received(1).GetSeat(eventSeatId);
+        }
+
+        [Test]
+        public async Task RemoveSeat_ValidSeat_RemovesSeat()
+        {
+            // Arrange
+            var bookingId = 1;
+            var eventSeatId = 1;
+            _bookingSeatRepository.GetSeat(eventSeatId).Returns(new BookingSeat { BookingId = bookingId });
+
+            // Act
+            await _service.RemoveSeat(bookingId, eventSeatId);
+
+            // Assert
+            await _bookingSeatRepository.Received(1).DeleteSeat(bookingId, eventSeatId);
+        }
+
+        [Test]
+        public async Task BookSeats_ValidSeats_UpdatesStatus()
+        {
+            // Arrange
+            var bookingId = 1;
+
+            // Act
+            await _service.BookSeats(bookingId);
+
+            // Assert
+            await _bookingSeatRepository.Received(1).UpdateSeatsStatus(bookingId, EventSeatStatus.Booked);
+        }
+    }
+}
