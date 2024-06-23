18bbd317ce0e4c36a9437ae507d1a26046a4bb27
Kirill Ovcharenko <kirill.056@gmail.com>
2024-06-23 18:18:28 +0300

    Add locks for concurrency handling in AddToCart
    Rework AddToCart endpoint to follow pessimistic concurrency strategy

--- EventTicketingSystem.API/Helpers/BusinessExceptionFilter.cs
+++ EventTicketingSystem.API/Helpers/BusinessExceptionFilter.cs
@@ -7,10 +7,18 @@ namespace EventTicketingSystem.API.Helpers
 {
     public class BusinessExceptionFilter : IExceptionFilter
     {
+        private readonly ILogger<BusinessExceptionFilter> _logger;
+
+        public BusinessExceptionFilter(ILogger<BusinessExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnException(ExceptionContext context)
         {
             if (context.Exception is BusinessException)
             {
+                _logger.LogError(context.Exception, context.Exception.Message);
                 context.Result = new JsonResult(context.Exception.Message)
                 {
                     StatusCode = (int)HttpStatusCode.BadRequest
--- EventTicketingSystem.API/Helpers/EntityNotFoundExceptionFilter.cs
+++ EventTicketingSystem.API/Helpers/EntityNotFoundExceptionFilter.cs
@@ -7,10 +7,18 @@ namespace EventTicketingSystem.API.Helpers
 {
     public class EntityNotFoundExceptionFilter : IExceptionFilter
     {
+        private readonly ILogger<EntityNotFoundExceptionFilter> _logger;
+
+        public EntityNotFoundExceptionFilter(ILogger<EntityNotFoundExceptionFilter> logger)
+        {
+            _logger = logger;
+        }
+
         public void OnException(ExceptionContext context)
         {
             if (context.Exception is EntityNotFoundException)
             {
+                _logger.LogError(context.Exception, context.Exception.Message);
                 context.Result = new JsonResult(context.Exception.Message)
                 {
                     StatusCode = (int)HttpStatusCode.NotFound,
